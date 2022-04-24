5595889d8f4f29b9e2171e4f2ca48b8533826942
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2022-04-24 23:06:45 +0500

    step 5

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,2 @@
 const menu = require("./module/menu.js");
+const popups = require("./module/popups.js");
---
+++ src/js/module/popups.js
@@ -0,0 +1,59 @@
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding')
+
+let unlock = true;
+
+const timeout = 800;
+
+for (let i = 0; i < popupLinks.length; i++) {
+    const popupLink = popupLinks[i];
+    popupLink.addEventListener('click', function(e) {
+        const popupName = popupLink.getAttribute('href').replace('#', '');
+        const currentPopup = document.getElementById(popupName);
+        popupOpen(currentPopup);
+        e.preventDefault();
+    })
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+// console.log(popupCloseIcon);
+
+if (popupCloseIcon.length > 0) {
+    for (let i = 0; i < popupCloseIcon.length; i++) {
+        const el = popupCloseIcon[i];
+        el.addEventListener('click', function(e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup && unlock) {
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false)
+        }
+
+        currentPopup.classList.add('open');
+        currentPopup.addEventListener('click', function(e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        })
+    }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove('open');
+    }
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open');
+        popupClose(popupActive);
+    }
+})
